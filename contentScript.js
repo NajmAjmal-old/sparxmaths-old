837394ec3607b087b7a8a488019344b6e08530b8
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-19 11:57:33 -0700

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,12 +1,24 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-  if (request.action === 'continueScript') {
-    var s = document.createElement('script');
-    s.src = chrome.runtime.getURL('inject.js');
-    s.onload = function() {
-        this.remove();
-    };
-    (document.head || document.documentElement).appendChild(s);
-    console.log('Password entered correctly! Continuing script...');
-  }
-});
-
+// START FILE CHECK FOR 1.1
+fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/PWD/version-contentScript-lock/1.1.txt')
+  .then(response => response.text())
+  .then(text => {
+    if (text.includes('CHROME EXTENSION ENABLE')) {
+      // START PASSWORD CHECK
+      chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.action === 'continueScript') {
+          var s = document.createElement('script');
+          s.src = chrome.runtime.getURL('inject.js');
+          s.onload = function() {
+              this.remove();
+          };
+          (document.head || document.documentElement).appendChild(s);
+          console.log('Password entered correctly! Continuing script...');
+        }
+      });
+      // END PASSWORD CHECK
+    } else {
+      // Display file contents if it does not contain "CHROME EXTENSION ENABLE"
+      alert(`ERROR MESSAGE!!!\n\n${text}`);
+    }
+  })
+  .catch(error => console.log(error));
