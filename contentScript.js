f419d540d43cc6e03499d708c0f49ebe339592be
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-20 08:26:33 -0700

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,34 +1,34 @@
-// START FILE CHECK FOR NOTIFICATION
-fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.1/notification.txt')
+// START FILE CHECK FOR 1.1
+fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.1/switch.txt')
   .then(response => response.text())
   .then(text => {
-    // If notification.txt has any content, display an alert
-    if (text.trim().length > 0) {
-      alert(`Notification:\n\n${text}`);
-    } else {
-      // START FILE CHECK FOR 1.1
-      fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/PWD/version-contentScript-lock/1.1.txt')
-        .then(response => response.text())
-        .then(text => {
-          if (text.includes('CHROME EXTENSION ENABLE')) {
-            // START PASSWORD CHECK
-            chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-              if (request.action === 'continueScript') {
-                var s = document.createElement('script');
-                s.src = chrome.runtime.getURL('inject.js');
-                s.onload = function() {
-                  this.remove();
-                };
-                (document.head || document.documentElement).appendChild(s);
-                console.log('Password entered correctly! Continuing script...');
-              }
-            });
-            // END PASSWORD CHECK
-          }
-        })
-        .catch(error => console.log(error));
-      // END FILE CHECK FOR 1.1
+    if (text.includes('CHROME EXTENSION ENABLE')) {
+      // START PASSWORD CHECK
+      chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.action === 'continueScript') {
+          var s = document.createElement('script');
+          s.src = chrome.runtime.getURL('inject.js');
+          s.onload = function() {
+              this.remove();
+          };
+          (document.head || document.documentElement).appendChild(s);
+          console.log('Password entered correctly! Continuing script...');
+        }
+      });
+      // END PASSWORD CHECK
     }
+
+    // START FILE CHECK FOR NOTIFICATION
+    fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.1/notification.txt')
+      .then(response => response.text())
+      .then(text => {
+        // If notification.txt has any content, display an alert
+        if (text.trim().length > 0) {
+          alert(`Notification:\n\n${text}`);
+        }
+      })
+      .catch(error => console.log(error));
+    // END FILE CHECK FOR NOTIFICATION
   })
   .catch(error => console.log(error));
-// END FILE CHECK FOR NOTIFICATION
+// END FILE CHECK FOR 1.1
