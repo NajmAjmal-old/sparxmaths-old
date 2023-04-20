caaae9bb3806a9f772ad524d0861f82a4e0efd34
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-20 12:04:57 -0700

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,34 +1,36 @@
-// START FILE CHECK FOR 1.2
-fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/switch.txt')
+// START FILE CHECK FOR NOTIFICATION
+fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/notification.txt')
   .then(response => response.text())
   .then(text => {
-    if (text.includes('CHROME EXTENSION ENABLE')) {
-      // START PASSWORD CHECK
-      chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        if (request.action === 'continueScript') {
-          var s = document.createElement('script');
-          s.src = chrome.runtime.getURL('inject.js');
-          s.onload = function() {
-              this.remove();
-          };
-          (document.head || document.documentElement).appendChild(s);
-          console.log('Password entered correctly! Continuing script...');
-        }
-      });
-      // END PASSWORD CHECK
+    // If notification.txt has any content, display an alert
+    if (text.trim().length > 0) {
+      alert(`Notification:\n\n${text}`);
+    } else {
+      // START FILE CHECK FOR PASSWORD
+      fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/PWD/version-contentScript-lock/1.2.txt')
+        .then(response => response.text())
+        .then(text => {
+          if (text.includes('CHROME EXTENSION ENABLE')) {
+            // START PASSWORD CHECK
+            chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+              if (request.action === 'continueScript') {
+                var s = document.createElement('script');
+                s.src = chrome.runtime.getURL('inject.js');
+                s.onload = function() {
+                    this.remove();
+                };
+                (document.head || document.documentElement).appendChild(s);
+                console.log('Password entered correctly! Continuing script...');
+              }
+            });
+            // END PASSWORD CHECK
+          } else {
+            console.log('Chrome extension is not enabled, script will not continue.');
+          }
+        })
+        .catch(error => console.log(error));
+      // END FILE CHECK FOR PASSWORD
     }
-
-    // START FILE CHECK FOR NOTIFICATION
-    fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/notification.txt')
-      .then(response => response.text())
-      .then(text => {
-        // If notification.txt has any content, display an alert
-        if (text.trim().length > 0) {
-          alert(`Notification:\n\n${text}`);
-        }
-      })
-      .catch(error => console.log(error));
-    // END FILE CHECK FOR NOTIFICATION
   })
   .catch(error => console.log(error));
-// END FILE CHECK FOR 1.2
+// END FILE CHECK FOR NOTIFICATION
