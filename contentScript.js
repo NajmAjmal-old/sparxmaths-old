76e876bf173ffd35487fdfbd61b2d960e98d0c42
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-20 23:11:47 -0700

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,36 +1,37 @@
-// START FILE CHECK FOR NOTIFICATION
-fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/notification.txt')
+// START FILE CHECK FOR EXTENSION ENABLEMENT
+fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/switch.txt')
   .then(response => response.text())
   .then(text => {
-    // If notification.txt has any content, display an alert
-    if (text.trim().length > 0) {
-      alert(`Notification:\n\n${text}`);
-    } else {
-      // START FILE CHECK FOR PASSWORD
-      fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/switch.txt')
+    if (text.includes('CHROME EXTENSION ENABLE')) {
+      // START FILE CHECK FOR NOTIFICATION
+      fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/notification.txt')
         .then(response => response.text())
         .then(text => {
-          if (text.includes('CHROME EXTENSION ENABLE')) {
-            // START PASSWORD CHECK
-            chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-              if (request.action === 'continueScript') {
-                var s = document.createElement('script');
-                s.src = chrome.runtime.getURL('inject.js');
-                s.onload = function() {
-                    this.remove();
-                };
-                (document.head || document.documentElement).appendChild(s);
-                console.log('Password entered correctly! Continuing script...');
-              }
-            });
-            // END PASSWORD CHECK
-          } else {
-            console.log('Chrome extension is not enabled, script will not continue.');
+          // If notification.txt has any content, display an alert
+          if (text.trim().length > 0) {
+            alert(`Notification:\n\n${text}`);
           }
         })
         .catch(error => console.log(error));
-      // END FILE CHECK FOR PASSWORD
+      // END FILE CHECK FOR NOTIFICATION
+
+      // START PASSWORD CHECK
+      chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+        if (request.action === 'continueScript') {
+          var s = document.createElement('script');
+          s.src = chrome.runtime.getURL('inject.js');
+          s.onload = function() {
+              this.remove();
+          };
+          (document.head || document.documentElement).appendChild(s);
+          console.log('Password entered correctly! Continuing script...');
+        }
+      });
+      // END PASSWORD CHECK
+
+    } else {
+      console.log('Chrome extension is not enabled, script will not continue.');
     }
   })
   .catch(error => console.log(error));
-// END FILE CHECK FOR NOTIFICATION
+// END FILE CHECK FOR EXTENSION ENABLEMENT
