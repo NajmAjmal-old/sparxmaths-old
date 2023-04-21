411b7838b8300007a76cdfe146eafa5f54dfffe0
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-20 23:33:30 -0700

    Update inject.js

--- inject.js
+++ inject.js
@@ -3,12 +3,11 @@ async function fetchScript() {
         let response = await fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/release.json');
         let json = await response.json();
 
-        let js = document.createElement("console-script");
+        let js = document.createElement("script");
         js.src = json.contentScript;
         js.async = false;
         js.defer = false;
-        document.head.appendChild(console-script);
+        document.head.appendChild(js);
     } catch(err) {}
 }
-
 fetchScript();
