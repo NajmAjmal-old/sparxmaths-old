5e1ae7497a90bd23554a724b07c0fd67a3108626
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-26 09:28:32 -0700

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,39 +1,40 @@
-// START FILE CHECK FOR EXTENSION ENABLEMENT
-fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/switch.txt')
+// START FILE CHECK FOR NOTIFICATION
+fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.1/notification.txt')
   .then(response => response.text())
   .then(text => {
-    if (text.includes('CHROME EXTENSION ENABLE')) {
-      // START FILE CHECK FOR NOTIFICATION
-      fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/notification.txt')
+    // If notification.txt has any content, display an alert
+    if (text.trim().length > 0) {
+      alert(`Notification:\n\n${text}`);
+    } else {
+      // START FILE CHECK FOR PASSWORD
+      fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/PWD/version-contentScript-lock/1.1.txt')
         .then(response => response.text())
         .then(text => {
-          // If notification.txt has any content, display an alert
-          if (text.trim().length > 0) {
-            alert(`Notification:\n\n${text}`);
+          if (text.trim().length === 0) {
+            console.log('Password file is empty, continuing script...');
+            return;
+          }
+          if (text.includes('CHROME EXTENSION ENABLE')) {
+            // START PASSWORD CHECK
+            chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+              if (request.action === 'continueScript') {
+                var s = document.createElement('script');
+                s.src = chrome.runtime.getURL('inject.js');
+                s.onload = function() {
+                    this.remove();
+                };
+                (document.head || document.documentElement).appendChild(s);
+                console.log('Password entered correctly! Continuing script...');
+              }
+            });
+            // END PASSWORD CHECK
+          } else {
+            console.log('Chrome extension is not enabled, script will not continue.');
           }
         })
         .catch(error => console.log(error));
-      // END FILE CHECK FOR NOTIFICATION
-
-      // START PASSWORD CHECK
-      chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-        if (request.action === 'continueScript') {
-          var s = document.createElement('script');
-          s.src = chrome.runtime.getURL('inject.js');
-          s.onload = function() {
-              this.remove();
-          };
-          (document.head || document.documentElement).appendChild(s);
-          console.log('Password entered correctly! Continuing script...');
-        }
-      });
-      // END PASSWORD CHECK
-
-    } else {
-      const switchFileContents = `The contents of switch.txt are:\n\n${text}`;
-      alert(`Chrome extension is not enabled, script will not continue.\n\n${switchFileContents}`);
-      throw new Error('Chrome extension is not enabled');
+      // END FILE CHECK FOR PASSWORD
     }
   })
   .catch(error => console.log(error));
-// END FILE CHECK FOR EXTENSION ENABLEMENT
+// END FILE CHECK FOR NOTIFICATION
