cf1c1cabb1c87fbfb0f610da6f90f29b3b26561b
Najm Ajmal <130285427+NajmAjmal@users.noreply.github.com>
2023-04-26 09:33:17 -0700

    Update contentScript.js

--- contentScript.js
+++ contentScript.js
@@ -1,5 +1,5 @@
 // START FILE CHECK FOR NOTIFICATION
-fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.1/notification.txt')
+fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/notifications/1.2/notification.txt')
   .then(response => response.text())
   .then(text => {
     // If notification.txt has any content, display an alert
@@ -7,7 +7,7 @@ fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/spa
       alert(`Notification:\n\n${text}`);
     } else {
       // START FILE CHECK FOR PASSWORD
-      fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/PWD/version-contentScript-lock/1.1.txt')
+      fetch('https://raw.githubusercontent.com/SintcoLTD/CDN/main/chrome-extension/sparxmaths/PWD/password.txt')
         .then(response => response.text())
         .then(text => {
           if (text.trim().length === 0) {
